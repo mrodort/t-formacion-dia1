90867505eda1a00bf16ddf68f581717dbddd4841
PC48\userPC49 <maysunderground@gmail.com>
2017-01-09 16:35:56 +0100

    feat. validar y permitir los comandos para seleccionar el archivo a leer

--- src/main/java/com/tsystem/dia1/world_csv/Aplication.java
+++ src/main/java/com/tsystem/dia1/world_csv/Aplication.java
@@ -7,19 +7,36 @@ import au.com.bytecode.opencsv.CSVReader;
 
 public class Aplication {
 
-    public static void main(String args[]) {
+    public static void main(final String args[]) {
+
+	if (args.length < 1) {
+	    throw new IllegalArgumentException("Debe a menos indecar un parametro para el comando.");
+	}
+
+	final ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+	File cvsFile = null;
+
+	switch (args[0]) {
+	case "city":
+	    cvsFile = new File(systemClassLoader.getResource("city.csv").getFile());
+	    break;
+	case "country":
+	    cvsFile = new File(systemClassLoader.getResource("country.csv").getFile());
+	    break;
+	case "language":
+	    cvsFile = new File(systemClassLoader.getResource("countrylanguage.csv").getFile());
+	    break;
+	default:
+	    throw new IllegalArgumentException("Argumento invalido, seleccione city, country o language");
+	}
 
 	try {
 
-	    ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
-	    File cityFile = new File(systemClassLoader.getResource("city.csv").getFile());
-
-	    CSVReader reader = new CSVReader(new FileReader(cityFile), ';');
+	    final CSVReader reader = new CSVReader(new FileReader(cvsFile), ';');
 	    String[] nextLine;
 	    while ((nextLine = reader.readNext()) != null) {
 		// nextLine[] is an array of values from the line
-		System.out.println(
-			nextLine[0] + " " + nextLine[1] + " " + nextLine[2] + " " + nextLine[3] + " " + nextLine[4]);
+		System.out.println(nextLine[0] + " " + nextLine[1] + " etc...");
 	    }
 
 	} catch (Exception e) {
