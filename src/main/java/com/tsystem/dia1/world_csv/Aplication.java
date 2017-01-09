d2a115019b43dfff00a5c78df4705ed6ac05c245
PC48\userPC49 <maysunderground@gmail.com>
2017-01-09 13:47:49 +0100

    feature cvs reader

--- src/main/java/com/tsystem/dia1/world_csv/Aplication.java
+++ src/main/java/com/tsystem/dia1/world_csv/Aplication.java
@@ -1,5 +1,29 @@
 package com.tsystem.dia1.world_csv;
 
+import java.io.File;
+import java.io.FileReader;
+
+import au.com.bytecode.opencsv.CSVReader;
+
 public class Aplication {
 
+    public static void main(String args[]) {
+
+	try {
+
+	    ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+	    File cityFile = new File(systemClassLoader.getResource("city.csv").getFile());
+
+	    CSVReader reader = new CSVReader(new FileReader(cityFile), ';');
+	    String[] nextLine;
+	    while ((nextLine = reader.readNext()) != null) {
+		// nextLine[] is an array of values from the line
+		System.out.println(
+			nextLine[0] + " " + nextLine[1] + " " + nextLine[2] + " " + nextLine[3] + " " + nextLine[4]);
+	    }
+
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+    }
 }
